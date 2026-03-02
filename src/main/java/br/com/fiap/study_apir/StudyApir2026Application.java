c7162481304d7ec8a9c87dab09e8bb26d634806b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-02 20:20:56 -0300

    Projeto inicializado

---
+++ src/main/java/br/com/fiap/study_apir/StudyApir2026Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.study_apir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApir2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApir2026Application.class, args);
+	}
+
+}
